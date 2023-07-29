e5972441e0e5af22371792760f2a128cd5cfffea
Esat GÖZCÜ <esatgozcu@gmail.com>
2023-07-30 02:44:39 +1000

    Example usage has been updated

--- app/src/main/java/com/esatgozcu/rollingnumber/MainActivity.kt
+++ app/src/main/java/com/esatgozcu/rollingnumber/MainActivity.kt
@@ -3,13 +3,19 @@ package com.esatgozcu.rollingnumber
 import android.os.Bundle
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
-import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.foundation.background
+import androidx.compose.foundation.clickable
+import androidx.compose.foundation.layout.*
 import androidx.compose.material.MaterialTheme
 import androidx.compose.material.Surface
 import androidx.compose.material.Text
 import androidx.compose.runtime.Composable
+import androidx.compose.runtime.mutableStateOf
+import androidx.compose.runtime.remember
+import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.tooling.preview.Preview
+import androidx.compose.ui.unit.dp
 import com.esatgozcu.rollingnumber.ui.theme.RollingNumberTheme
 
 class MainActivity : ComponentActivity() {
@@ -20,7 +26,7 @@ class MainActivity : ComponentActivity() {
                 // A surface container using the 'background' color from the theme
                 Surface(modifier = Modifier.fillMaxSize(),
                     color = MaterialTheme.colors.background) {
-                    Greeting("Android")
+                    Greeting()
                 }
             }
         }
@@ -28,14 +34,24 @@ class MainActivity : ComponentActivity() {
 }
 
 @Composable
-fun Greeting(name: String) {
-    RollingNumberView(vm = RollingNumberVM("123","Esat : "))
+fun Greeting() {
+    val number = remember { mutableStateOf(100) }
+    Column(
+        horizontalAlignment = Alignment.CenterHorizontally,
+        verticalArrangement = Arrangement.Center
+    ) {
+        RollingNumberView(vm = RollingNumberVM(number.value.toString(), suffix = "$"))
+        Spacer(modifier = Modifier.height(10.dp))
+        Text(text = "Increase",Modifier.clickable {
+            number.value = number.value + 123
+        })
+    }
 }
 
 @Preview(showBackground = true)
 @Composable
 fun DefaultPreview() {
     RollingNumberTheme {
-        Greeting("Android")
+        Greeting()
     }
 }
